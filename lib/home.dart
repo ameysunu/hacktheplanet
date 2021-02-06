6e3bf50763c1b21c5956582b71cc6a7de618ceba
Amey Sunu <ameysunu00719@gmail.com>
2021-02-06 11:20:23 +0300

    multidex added

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hacktheplanet/login.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:intl/intl.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 var time = DateTime.now().hour;
 var min = DateTime.now().minute;
