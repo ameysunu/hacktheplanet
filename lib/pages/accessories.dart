2abfb9cd3e0e34f220362553974bdd7d1c211d9c
Amey Sunu <ameysunu00719@gmail.com>
2021-02-16 22:50:41 +0300

    firestore added

--- lib/pages/accessories.dart
+++ lib/pages/accessories.dart
@@ -1,16 +1,86 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:flutter/material.dart';
 
+final firestoreInstance = FirebaseFirestore.instance;
+AsyncSnapshot<DocumentSnapshot> snapshot;
+Stream<QuerySnapshot> groceryStream;
+
 class Accessories extends StatefulWidget {
+  final String name, price, place;
+  Accessories({Key key, this.name, this.price, this.place}) : super(key: key);
   @override
   _AccessoriesState createState() => _AccessoriesState();
 }
 
 class _AccessoriesState extends State<Accessories> {
   @override
+  void initState() {
+    super.initState();
+    groceryStream = firestoreInstance.collection('accessory').snapshots();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: HexColor('#FFE3EA'),
-    );
+        backgroundColor: HexColor('#FFE3EA'),
+        body: StreamBuilder(
+            stream: groceryStream,
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              var totalgroupCount = 0;
+              List<DocumentSnapshot> groupUsers;
+              if (snapshot.hasData) {
+                groupUsers = snapshot.data.documents;
+                totalgroupCount = groupUsers.length;
+                return GridView.builder(
+                    itemCount: totalgroupCount,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2),
+                    itemBuilder: (BuildContext context, int index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              // image: DecorationImage(
+                              //     fit: BoxFit.cover,
+                              //     image: NetworkImage(
+                              //         groupUsers[index].data()['image'])),
+                              color: HexColor('#EC1C64'),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(15))),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Spacer(),
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Text(
+                                  groupUsers[index].data()['name'],
+                                  style: TextStyle(
+                                      fontFamily: 'Gotham',
+                                      fontSize: 17,
+                                      color: HexColor('#FFE3EA')),
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.fromLTRB(10, 0, 10, 10),
+                                child: Text(
+                                  groupUsers[index].data()['price'],
+                                  style: TextStyle(
+                                      fontFamily: 'Gotham',
+                                      fontSize: 17,
+                                      color: HexColor('#FFE3EA')),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    });
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            }));
   }
 }
