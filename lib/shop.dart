5279dd062385108c87720e170630d9f2a85832fd
Amey Sunu <ameysunu00719@gmail.com>
2021-02-16 12:15:31 +0300

    tab added

--- lib/shop.dart
+++ lib/shop.dart
@@ -9,8 +9,32 @@ class Shop extends StatefulWidget {
 class _ShopState extends State<Shop> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: HexColor('#FFE3EA'),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        backgroundColor: HexColor('#FFE3EA'),
+        appBar: AppBar(
+            automaticallyImplyLeading: false,
+            backgroundColor: HexColor('#FFE3EA'),
+            elevation: 0,
+            bottom: TabBar(tabs: [
+              Tab(
+                icon: Text(
+                  "Grocery".toUpperCase(),
+                  style: TextStyle(
+                      fontFamily: 'Gotham', color: HexColor('#EC1C64')),
+                ),
+              ),
+              Tab(
+                icon: Text(
+                  "Accessories".toUpperCase(),
+                  style: TextStyle(
+                      fontFamily: 'Gotham'.toUpperCase(),
+                      color: HexColor('#EC1C64')),
+                ),
+              ),
+            ])),
+      ),
     );
   }
 }
