0d6b555870c8b69a18fdb732856ba8bcd5d61b44
Amey Sunu <ameysunu00719@gmail.com>
2021-02-16 14:14:57 +0300

    null error fixed

--- lib/pages/grocery.dart
+++ lib/pages/grocery.dart
@@ -29,55 +29,59 @@ class _GroceryState extends State<Grocery> {
             builder: (BuildContext context, AsyncSnapshot snapshot) {
               var totalgroupCount = 0;
               List<DocumentSnapshot> groupUsers;
-              groupUsers = snapshot.data.documents;
-              totalgroupCount = groupUsers.length;
-              if (!snapshot.hasData) {
-                return Center(
-                  child: Text("Whoopsie!"),
-                );
-              }
-              return GridView.builder(
-                  itemCount: totalgroupCount,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2),
-                  itemBuilder: (BuildContext context, int index) {
-                    // return Text(groupUsers[index].data()['title']);
-                    return Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            color: HexColor('#EC1C64'),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(15))),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Spacer(),
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Text(
-                                groupUsers[index].data()['name'],
-                                style: TextStyle(
-                                    fontFamily: 'Gotham',
-                                    fontSize: 17,
-                                    color: HexColor('#FFE3EA')),
+              if (snapshot.hasData) {
+                groupUsers = snapshot.data.documents;
+                totalgroupCount = groupUsers.length;
+                return GridView.builder(
+                    itemCount: totalgroupCount,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2),
+                    itemBuilder: (BuildContext context, int index) {
+                      // return Text(groupUsers[index].data()['title']);
+                      return Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: NetworkImage(
+                                      groupUsers[index].data()['image'])),
+                              color: HexColor('#EC1C64'),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(15))),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Spacer(),
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Text(
+                                  groupUsers[index].data()['name'],
+                                  style: TextStyle(
+                                      fontFamily: 'Gotham',
+                                      fontSize: 17,
+                                      color: HexColor('#FFE3EA')),
+                                ),
                               ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
-                              child: Text(
-                                groupUsers[index].data()['price'],
-                                style: TextStyle(
-                                    fontFamily: 'Gotham',
-                                    fontSize: 17,
-                                    color: HexColor('#FFE3EA')),
-                              ),
-                            )
-                          ],
+                              Padding(
+                                padding:
+                                    const EdgeInsets.fromLTRB(10, 0, 10, 10),
+                                child: Text(
+                                  groupUsers[index].data()['price'],
+                                  style: TextStyle(
+                                      fontFamily: 'Gotham',
+                                      fontSize: 17,
+                                      color: HexColor('#FFE3EA')),
+                                ),
+                              )
+                            ],
+                          ),
                         ),
-                      ),
-                    );
-                  });
+                      );
+                    });
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
             }));
   }
 }
