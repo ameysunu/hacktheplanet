ffec58e57ce82e01187012e403950b6847cb0bf9
Amey Sunu <ameysunu00719@gmail.com>
2021-02-18 12:08:46 +0300

    donate added

--- lib/donate.dart
+++ lib/donate.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class Donate extends StatefulWidget {
   @override
@@ -10,6 +11,99 @@ class _DonateState extends State<Donate> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.pink,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.pink,
+      ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * 0.05,
+            ),
+            Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  "Want to donate?",
+                  style: TextStyle(
+                      color: HexColor('#FFE3EA'),
+                      fontFamily: 'Gotham',
+                      fontSize: 25),
+                )),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.05,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: [
+                      RawMaterialButton(
+                        onPressed: () {},
+                        fillColor: HexColor('#C33589'),
+                        child: Icon(
+                          Icons.food_bank,
+                          size: 50.0,
+                          color: HexColor('#FFE3EA'),
+                        ),
+                        padding: EdgeInsets.all(15.0),
+                        shape: CircleBorder(),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Text(
+                          "Food",
+                          style: TextStyle(
+                            fontFamily: 'Gotham',
+                            color: HexColor('#FFE3EA'),
+                            fontSize: 17,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      RawMaterialButton(
+                        onPressed: () {},
+                        fillColor: HexColor('#C33589'),
+                        child: Icon(
+                          Icons.local_grocery_store,
+                          size: 50.0,
+                          color: HexColor('#FFE3EA'),
+                        ),
+                        padding: EdgeInsets.all(15.0),
+                        shape: CircleBorder(),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Text(
+                          "Grocery",
+                          style: TextStyle(
+                            fontFamily: 'Gotham',
+                            color: HexColor('#FFE3EA'),
+                            fontSize: 17,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  child: Image.asset('images/donate.png')),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
